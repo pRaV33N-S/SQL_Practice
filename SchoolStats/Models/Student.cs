4ab941ca7ef77ab6f23c88132001e6bcea965d29
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-09-07 12:31:05 +0530

    Section 7

---
+++ SchoolStats/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolStats.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [Required]
+        public string StudentName { get; set; } 
+        public int TotalMarks { get; set; }
+    }
+}
---
+++ SchoolStats/Models/StudentStatistics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolStats.Models
+{
+    public class StudentStatistics
+    {
+        public int StudentCount { get; set; }
+        public Student MaxMarkStudent { get; set; }
+        public Student MinMarkStudent { get; set; }
+        public List<Student> StudentsWithLessThan50Marks { get; set; }
+        public double AverageMark { get; set; }
+    }
+}
